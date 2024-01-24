8c85bd72646c20b32e803f4238bb2515adeab72a
K0tana <yermekov117@mail.ru>
2024-01-24 16:52:55 +0600

    Beta1.01

--- lab_1.py
+++ lab_1.py
@@ -2,31 +2,44 @@
 print("Hello World")
 if 5 >= 4:
     print("YES")
+
+
 # comments
 """THE FIRST AVATAR MOVIE WASN'T SO GOOD AS PEOPLE SAY """
+""" This if comment too"""
+
 # variables
-a, b = 5, 6
-print(a + b)
-s = "gigitigag"
-print(s * 3)
+a1, b1 = 5, 6
 a = b, c = d = 5, 6
+s = "gigitigag"
+print(a1 + b1)
+print(s * 3)
 print(s, "seems you win {}".format(input("\nyour name:")))
 print(a, b, c, d)
+
+
 # numbers
 a = 5.456342
 print(a)
 print(int(a))
 print(complex(a))
 print(a - int(a) + complex(a))
-# data types
+
+
+# random variables
 a = "textes"
 b = ["a", "b", "c"]
 c = 20.5
 d = 5 + 3j
-# data types
 e = {"class" : "Autobot", "weapon": "glass gas cannon", "name": "clifjumper"}
+
+
+# data types
+
 f = [a, b, c, d, e]
 print([type(i) for i in f])
+
+
 # strings
 p = "hello world"
 print(p.lower())
